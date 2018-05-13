e024f6f84c74bdeb1c1285165f66df6ec759233a
linguowang007 <linguowang007@163.com>
2018-05-13 16:27:14 +0800

    modify the count_q30.py

--- count_q30.py
+++ count_q30.py
@@ -18,6 +18,6 @@ with gzip.open(fastq, 'rt') as f:
 total_base = sum(c1.values())
 q10 = sum(value for key, value in c1.items() if ord(key) - 43 >= 0)  # q10 base count
 q20 = sum(value for key, value in c1.items() if ord(key) - 53 >= 0)  # q20 base count
-q30 = sum(value for key, value in c1.items() if ord(key) - 63 >= 0)  # q 30 base count
+q30 = sum(value for key, value in c1.items() if ord(key) - 63 >= 0)  # q30 base count
 
 print(fastq, total_base, q10, q20, q30, sep='\t')  # print the fastq file, total base, q10 , q20 , q30 to stander output
